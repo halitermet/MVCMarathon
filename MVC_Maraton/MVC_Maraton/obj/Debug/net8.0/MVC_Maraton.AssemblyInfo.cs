935fc0dc01685868a05857e6585c45690dcd96b8
Turker Ayan <147872643+turkerayan@users.noreply.github.com>
2024-03-25 09:31:31 +0300

    gitignore2

--- MVC_Maraton/MVC_Maraton/obj/Debug/net8.0/MVC_Maraton.AssemblyInfo.cs
+++ MVC_Maraton/MVC_Maraton/obj/Debug/net8.0/MVC_Maraton.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_Maraton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+333accf7bb47e085578702728e43b7d4278c898b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_Maraton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_Maraton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
